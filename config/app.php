1e956a847f993be585fbde0e5203c6f554dfc91d
lupael <lupael@i4e.com.bd>
2026-02-08 03:54:43 +0000

    I've registered the TenancyServiceProvider in config/app.php

--- config/app.php
+++ config/app.php
@@ -153,4 +153,20 @@ return [
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
 
+    /*
+    |--------------------------------------------------------------------------
+    | Autoloaded Service Providers
+    |--------------------------------------------------------------------------
+    |
+    | The service providers listed here will be automatically loaded on the
+    | request to your application. Feel free to add your own services to
+    | this array to grant expanded functionality to your applications.
+    |
+    */
+
+    'providers' => [
+        // Other Service Providers
+        App\Providers\TenancyServiceProvider::class,
+    ],
+
 ];
